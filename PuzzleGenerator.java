48fd1b7b493cb6318b1bc862bdfc90de0b6073db
AnasYousaf-dev <anasyouasf440@gmail.com>
2026-01-15 09:11:53 +0500

    Convert code into Modules

--- PuzzleGenerator.java
+++ PuzzleGenerator.java
@@ -13,7 +13,6 @@ class PuzzleGenerator {
         remove(grid, 40);
         return grid;
     }
-
     private static void fillDiagonal(int[][] grid) {
         for (int i = 0; i < SIZE; i += SUBGRID) fillBox(grid, i, i);
     }
